faaa32ad5f138d45a51238da033bd5f9b8da809b
Alex <alex.iwdagency@gmail.com>
2018-06-28 23:29:03 +0300

    create extension

---
+++ Block/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace Custom\ImageOptimization\Block;
+
+class Test extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
